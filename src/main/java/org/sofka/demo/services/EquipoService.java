e85c8be3745243fab312f19feea0f23cb3793588
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-07-13 17:05:48 -0500

    Service Creado: se crea el service para el equipo

---
+++ src/main/java/org/sofka/demo/services/EquipoService.java
@@ -0,0 +1,16 @@
+package org.sofka.demo.services;
+
+import org.sofka.demo.models.Equipo;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface EquipoService {
+
+    public Flux<Equipo> findAll();
+
+    public Mono<Equipo> findByCountry(String country);
+
+    public Mono<Equipo> save(Equipo equipo);
+
+    public Mono<Void> delete(Equipo equipo);
+}
