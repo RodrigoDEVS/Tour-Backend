1fd3df62cbd4cb857137630e1c3b61d7683826b8
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-07-13 16:58:19 -0500

    Repository Creado: se crea el repository para el equipo

---
+++ src/main/java/org/sofka/demo/repository/EquipoRepository.java
@@ -0,0 +1,6 @@
+package org.sofka.demo.repository;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface EquipoRepository extends ReactiveMongoRepository {
+}
