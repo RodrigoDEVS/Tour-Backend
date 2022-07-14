1d4b969bfc9d00fdbb880613f53eef25371fafb1
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-07-14 15:17:33 -0500

    Entidad Agregada: se agrega la entidad Ciclista

---
+++ src/main/java/org/sofka/demo/models/Ciclista.java
@@ -0,0 +1,17 @@
+package org.sofka.demo.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "cyclist")
+public class Ciclista {
+
+    @Id
+    private String id;
+    private String name;
+    private String number;
+    private String teamCode;
+    private String country;
+}
