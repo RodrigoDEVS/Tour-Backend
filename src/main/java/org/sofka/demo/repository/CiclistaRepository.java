e9210fcf87a13535c7bf95e0e078b88f46b76a4b
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-07-14 15:20:44 -0500

    Repositorio Agregado: se agrega el repository para Ciclista

---
+++ src/main/java/org/sofka/demo/repository/CiclistaRepository.java
@@ -0,0 +1,10 @@
+package org.sofka.demo.repository;
+
+import org.sofka.demo.models.Ciclista;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+
+public interface CiclistaRepository extends ReactiveMongoRepository<Ciclista, String> {
+
+    Flux<Ciclista> findByTeamCode(String teamCode);
+}
